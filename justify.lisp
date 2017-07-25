6c59ba09befec56c62948a90cc81b061c9706c76
dan <dan@marceline>
2017-07-25 08:25:01 +0100

    Fix hyphenation for justify add unit tests

--- justify.lisp
+++ justify.lisp
@@ -18,6 +18,12 @@
 	     (incf w))
 	w)))
 
+(defun last-char (str)
+  (let ((len (1- (length str))))
+    (if (>= len 0)
+	(char str len)
+	nil)))
+
 ;;sw function gets the width at the y specified y position,
 ;;this is so we can draw up to the edge of an image
 (defun justify (s &key (dy 11) (sw #'test-sw) (width #'test-width))
@@ -25,16 +31,18 @@
 	(i 0)
 	(x 0)
 	(y 0))
-    (labels ((is-space (c) (or
-			    (char= c #\Newline)
-			    (char= c #\ )))
+    (labels ((is-space (c)
+	       (or
+		(char= c #\Newline)
+		(char= c #\ )))
 	     (next-word ()
 	       (let ((w (make-array 16 
 				    :element-type 'standard-char 
 				    :fill-pointer 0
 				    :adjustable t)))
 		 (loop while (and (< i len)
-				  (not (is-space (aref s i))))
+				  (not (is-space (aref s i)))
+				  (not (eq #\- (last-char w))))
 		    do
 		      (vector-push-extend (aref s i) w)
 		      (incf i))
@@ -48,32 +56,33 @@
 	    (js (make-array 16 :element-type 'standard-char
 			    :fill-pointer 0
 			    :adjustable t)))
-	(loop while t do
-	     (let ((w (next-word)))
-	       (unless w
-		 (return))
-	       (let* ((dx (funcall width w))
-		      (fits (<= (+ dx x) (funcall sw y))))
-		 (if fits
-		     (progn
-		       (unless (= x 0)
-			 (vector-push-extend #\  js))
-		       (incf x dx))
-		     (progn
-		       (vector-push-extend #\Newline js)
-		       (setf x dx)
-		       (incf y dy)))
-	       (incf x spc)
-	       (loop for c across w do
-		    (vector-push-extend c js)))))
-	js))))
+	(let ((hyphenated nil))
+	  (loop while t do
+	       (let ((w (next-word)))
+		 (unless w
+		   (return))
+		 (let* ((dx (funcall width w))
+			(fits (<= (+ dx x) (funcall sw y))))
+		   (if fits
+		       (progn
+			 (unless (or (= x 0) hyphenated)
+			   (vector-push-extend #\  js))
+			 (unless hyphenated
+			   (incf x dx)))
+		       (progn
+			 (vector-push-extend #\Newline js)
+			 (setf x dx)
+			 (incf y dy)))
+		   (incf x spc)
+		   (loop for c across w do
+			(vector-push-extend c js)))
+		 (setf hyphenated (eq #\- (last-char w))))))
+	  js))))
 
 (defun justify-with-image (s imgw imgh font)
   (justify s :sw #'(lambda (y)
 		     (- +screen-width+ (if (<= y (1+ imgh)) imgw 0)))
 	   :width (font-width font)))
-
-
 
 (defparameter *odyssey*
 "Tell me, O muse, of that ingenious hero who travelled far and wide after he had sacked
@@ -83,4 +92,25 @@ save his own life and bring his men safely home, but do what he might he could n
 his men, for they perished through their own sheer folly in eating the cattle of the
 Sun-god Hyperion, so the god prevented them from ever reaching home. Tell me, too,
 about all these things, O daughter of Jove, from whatsoever source you may know them.")
+
+(defun justify-test (string expected)
+  (assert (string= (justify string
+			    :sw #'(lambda (x) (declare (ignorable x)) 64))
+		   expected)))
+
+(justify-test "Hy-phen" "Hy-phen")
+(justify-test "Fil line" "Fil line")
+(justify-test "Wrap simple" "Wrap
+simple")
+(justify-test "Wrap-hyphen" "Wrap-
+hyphen")
+(justify-test "Wrap-hyphen x" "Wrap-
+hyphen x")
+(justify-test "Nine chars" "Nine
+chars")
+(justify-test "Hy-p yes" "Hy-p yes")
+(justify-test "Wrap then fil" "Wrap
+then fil")
+
+
 

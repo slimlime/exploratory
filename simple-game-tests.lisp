c2a8cebbcb48b6c38d19c1d8fc4f91827d865710
dan <dan@marceline>
2018-02-24 22:44:10 +0000

    Dungeon cell now uses objects

--- simple-game-tests.lisp
+++ simple-game-tests.lisp
@@ -2,61 +2,73 @@
 ;;;TESTING;;;
 ;;;;;;;;;;;;;
 
-(defun run-tests (&optional (warn-only t))
+(defparameter *warn-only* t)
+
+(defmacro test (description &body body)
+  `(progn
+     (monitor-reset :start)
+     (monitor-run :break-on 'BRK :print nil)
+     (handler-case
+	 (progn
+	   ,@body)
+       (error (e)
+	 (let ((msg (format nil "~a FAILED~%~t'~a'" ,description e)))
+	   (if *warn-only*
+	       (format t "~a~%" msg)
+	       (assert nil nil msg)))))))
+
+(defun run-tests ()
   (run-simple-game :print nil)
   (terpri)
-  (macrolet ((test (description &body body)
-		   `(progn
-		      (monitor-reset :start)
-		      (monitor-run :break-on 'BRK :print nil)
-		      (handler-case
-			  (progn
-			    ,@body)
-			(error (e)
-			  (let ((msg (format nil "~a FAILED~%~t'~a'" ,description e)))
-			    (if warn-only
-			        (format t "~a~%" msg)
-				(assert nil nil msg))))))))
+  
     (test "Examining slime"
 	  (assert-clr :slime-examined)
 	  (test-input "EXAMINE SLIME")
 	  (assert-set :slime-examined))
+
     (test "Can't take the slime"
 	  (assert-object-in "REPELLENT SLIME" :dungeon-cell)
 	  (test-input "TAKE SLIME")
 	  (assert-object-in "REPELLENT SLIME" :dungeon-cell))
+
     (test "Poking out the blockage requires bone"
 	  (assert-set :lock-jammed)
 	  (test-input "POKE BONE IN LOCK")
 	  (assert-set :lock-jammed))
+
     (test "Taking and dropping the bone, qualified with adjective"
 	  (assert-object-in "FINGER BONE" :dungeon-cell)
 	  (test-input "TAKE FINGER BONE")
 	  (assert-object-in "FINGER BONE" :inventory)
 	  (test-input "DROP FINGER BONE")
 	  (assert-object-in "FINGER BONE" :dungeon-cell))
+
     (test "Taking and dropping the bone, noun only"
 	  (assert-object-in "FINGER BONE" :dungeon-cell)
 	  (test-input "TAKE BONE")
 	  (assert-object-in "FINGER BONE" :inventory)
 	  (test-input "DROP BONE")
 	  (assert-object-in "FINGER BONE" :dungeon-cell))
+
     (test "Taking and dropping the bone, as adjunct noun"
 	  (assert-object-in "FINGER BONE" :dungeon-cell)
 	  (test-input "TAKE FINGER")
 	  (assert-object-in "FINGER BONE" :inventory)
 	  (test-input "DROP FINGER")
 	  (assert-object-in "FINGER BONE" :dungeon-cell))
+
     (test "Poking out the blockage and losing the bone"
 	  (assert-set :lock-jammed)
 	  (test-input "TAKE BONE" "POKE BONE IN LOCK")
 	  (assert-clr :lock-jammed)
 	  (assert-object-in "FINGER BONE" :nowhere))
+
     (test "Not poking out the blockage and not losing the bone"
 	  (assert-set :lock-jammed)
 	  (test-input "POKE BONE IN LOCK")
 	  (assert-set :lock-jammed)
 	  (assert-object-in "FINGER BONE" :dungeon-cell))
+
     (test "Examining the floor"
 	  (assert-object-in "SHINY KEY" :nowhere)
 	  (assert-clr :floor-examined)
@@ -64,6 +76,7 @@
 	  (assert-set :floor-examined)
 	  (assert-clr :gorilla-seen)
 	  (assert-clr :sought-gorilla))
+
     (test "Examining the crack"
 	  (assert-clr :maya)
 	  (test-input "EXAMINE CRACK")
@@ -74,19 +87,34 @@
 	  (assert-object-in "SHINY KEY" :nowhere)
 	  (test-input "EXAMINE CRACK")
 	  (assert-object-in "SHINY KEY" :dungeon-cell))
+
     (test "The window"
 	  (assert-clr :door-open)
 	  (assert-object-in "INEDIBLE SLOP" :nowhere)
 	  (test-input "BANG WINDOW")
-	  (assert-object-in "INEDIBLE SLOP" :dungeon-cell)
-	  (assert-set :lock-jammed))
+	  (assert-object-in "INEDIBLE SLOP" :dungeon-cell))
+
+    (test "Can take key"
+      (test-input "LICK SLIME" "EXAMINE FLOOR" "EXAMINE CRACK" "TAKE KEY")
+      (assert-object-in "SHINY KEY" :inventory))
+
+    (test "Can bang on door and get slop"
+      (assert-object-in "INEDIBLE SLOP" :nowhere)
+      (test-input "BANG DOOR")
+      (assert-object-in "INEDIBLE SLOP" :dungeon-cell))
+
+    (test "Eating the slop makes it go away"
+      (test-input "BANG DOOR" "EAT SLOP")
+      (assert-object-in "INEDIBLE SLOP" :nowhere))
+    
     (test "Can escape"
 	  (test-input "LICK SLIME" "EXAMINE FLOOR" "EXAMINE CRACK"
 		      "TAKE KEY" "TAKE FINGER BONE" "POKE BONE IN LOCK"
 		      "KNOCK DOOR" "UNLOCK DOOR" "OPEN DOOR" "USE DOOR")
-	  (assert-location :corridor))))
+	  (assert-location :corridor)))
 
-(time
- (progn
-   (run-tests)
-   (terpri)))
+(let ((*warn-only* nil))
+  (time
+   (progn
+     (run-tests)
+     (terpri))))
